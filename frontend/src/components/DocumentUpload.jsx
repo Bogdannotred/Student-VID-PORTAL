a23caf63783f619d4af461f85da4b0b0e012ccc7
Bogdannotred <96499764+Bogdannotred@users.noreply.github.com>
2025-12-10 19:26:45 +0200

    upload

--- frontend/src/components/DocumentUpload.jsx
+++ frontend/src/components/DocumentUpload.jsx
@@ -1,11 +1,40 @@
-import React , { useEffect } from 'react'
+import React , { useEffect , useState } from 'react'
 import { supabase } from '../supabaseClient';
 
 export default function DocumentUpload () {
 
+    const [image , setImage] = useState(null)
+    const [file , setFile] = useState(null)
+    const [previewUrl , setPreviewUrl] = useState(null)
+
+    const handleUpload = {
+
+    }
+    const handleFileChange = (e) => {
+        const selected = e.target.files[0];
+        setFile(selected);
+        setPreviewUrl(URL.createObjectURL(selected));
+    }
+
     return(
-        <div className="bg-gray-300 text-black rounded-xl shadow-xl w-1/2 flex jusify-center">
-            Upload
+    <div className="mb-1">
+        Image <span className="font-css top">*</span>
+        <div className="">
+            <input 
+            type="file" 
+            id="file-input" 
+            name="ImageStyle"
+            onChange={handleFileChange}
+            />
         </div>
+            <img
+            alt="not found"
+            width={"250px"}
+            src={previewUrl}
+          />
+        <button onClick={handleUpload}>
+            Upload
+        </button>
+    </div>
     );
 }
